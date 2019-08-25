ae7f41bfddbb174b6459be3ca6890b83c684d137
kyamawaki <yamawaki.mob@gmail.com>
2019-08-25 20:03:05 +0900

    create dropdown jquery

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -7,24 +7,34 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WebAppTest.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebAppTest.Controllers
 {
+    public class SampModel
+    {
+    }
+
     public class Sample
     {
-        public List<TestTable> TestTables { get; set; }
+        [Range(1,10, ErrorMessage ="NO")]
+        public int Value1 { get; set; }
+        public List<string> List { get; set; }
+
+        public Sample()
+        {
+            this.List = new List<string>(new string [] { "1", "5", "10" });
+        }
     };
 
     public class TestController : Controller
     {
-        private TestEntities1 db = new TestEntities1();
-
         private Sample sample = new Sample();
 
         // GET: Test
         public ActionResult Index()
         {
-            this.sample.TestTables = db.TestTable.ToList();
+            ViewBag.List = new SelectList(this.sample.List);
             return View(sample);
         }
 
@@ -35,113 +45,15 @@ namespace WebAppTest.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            TestTable testTable = db.TestTable.Find(id);
-            if (testTable == null)
-            {
-                return HttpNotFound();
-            }
-            return View(testTable);
-        }
-
-        // GET: Test/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Test/Create
-        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
-        // 詳細については、https://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Name,Value")] TestTable testTable)
-        {
-            if (ModelState.IsValid)
-            {
-                db.TestTable.Add(testTable);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(testTable);
-        }
-
-        // GET: Test/Edit/5
-        public ActionResult Edit(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            TestTable testTable = db.TestTable.Find(id);
-            if (testTable == null)
-            {
-                return HttpNotFound();
-            }
-            return View(testTable);
-        }
-
-        // POST: Test/Edit/5
-        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
-        // 詳細については、https://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Name,Value")] TestTable testTable)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(testTable).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(testTable);
-        }
-
-        // GET: Test/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            TestTable testTable = db.TestTable.Find(id);
-            if (testTable == null)
-            {
-                return HttpNotFound();
-            }
-            return View(testTable);
-        }
-
-        // POST: Test/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            TestTable testTable = db.TestTable.Find(id);
-            db.TestTable.Remove(testTable);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            return View(sample);
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                db.Dispose();
             }
             base.Dispose(disposing);
-        }
-
-        [HttpPost, ActionName("Save")]
-        [ValidateAntiForgeryToken]
-        public ActionResult Save(Sample model)
-        {
-            for(int i=0; i<model.TestTables.Count; i++)
-            {
-                this.sample.TestTables[i].Value = model.TestTables[i].Value;
-            }
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
     }
 }
